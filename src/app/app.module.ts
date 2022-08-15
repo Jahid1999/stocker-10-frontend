6d6f586750e56cb494dd9336bfa517ecd64e525c
mh-sun <bsse1025@iit.du.ac.bd>
2022-08-16 00:11:05 +0600

    Category-Compare graph added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { CategoryCompareComponent } from './feature/components/category-compare/category-compare.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryCompareComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
