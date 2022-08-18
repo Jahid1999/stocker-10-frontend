13fe924bd1abe5584efede0eaa739c91b8553908
arman18 <bsse1029@iit.du.ac.bd>
2022-08-18 16:02:58 +0600

    table-fixed

--- src/app/home-table/home-table.component.ts
+++ src/app/home-table/home-table.component.ts
@@ -33,8 +33,14 @@ export class HomeTableComponent implements OnInit {
    ngOnInit(): void {
 
      this.dtOptions = {
-      pageLength: 2,
-       data:[{"trading_code":"First Janata Bank Mutual Fund","ltp":6.2,"closep":6.2,"change":-0.1,"ycp":6.3},{"trading_code":"Prime Finance First Mutual Fund","ltp":17.2,"closep":17.2,"change":-0.1,"ycp":17.3},{"trading_code":"aamra networks limited","ltp":39,"closep":39,"change":0.3,"ycp":38.7},{"trading_code":"aamra technologies limited","ltp":35.8,"closep":35.8,"change":0.2,"ycp":35.6},{"trading_code":"AB Bank 1st Mutual fund","ltp":5.6,"closep":5.6,"change":0,"ycp":5.6},{"trading_code":"AB Bank Limited","ltp":10,"closep":10,"change":0,"ycp":10},{"trading_code":"Aman Cotton Fibrous Limited","ltp":27.7,"closep":27.7,"change":-0.3,"ycp":28},{"trading_code":"ACI  Limited","ltp":278.3,"closep":278.3,"change":3,"ycp":275.3}],
+      searching:false,
+      lengthChange:false,
+      language: {
+        paginate:{next: ">",last:"Last",first:"First",previous:"<"}
+      },
+      pageLength: 8,
+      columnDefs:[{ "width": "30%", "targets":  [0]},{"name":'some name',"targets":0},{"orderable":true,"targets":[0,1]}],
+      //  data:[{"trading_code":"First Janata Bank Mutual Fund","ltp":6.2,"closep":6.2,"change":-0.1,"ycp":6.3},{"trading_code":"Prime Finance First Mutual Fund","ltp":17.2,"closep":17.2,"change":-0.1,"ycp":17.3},{"trading_code":"aamra networks limited","ltp":39,"closep":39,"change":0.3,"ycp":38.7},{"trading_code":"aamra technologies limited","ltp":35.8,"closep":35.8,"change":0.2,"ycp":35.6},{"trading_code":"AB Bank 1st Mutual fund","ltp":5.6,"closep":5.6,"change":0,"ycp":5.6},{"trading_code":"AB Bank Limited","ltp":10,"closep":10,"change":0,"ycp":10},{"trading_code":"Aman Cotton Fibrous Limited","ltp":27.7,"closep":27.7,"change":-0.3,"ycp":28},{"trading_code":"ACI  Limited","ltp":278.3,"closep":278.3,"change":3,"ycp":275.3}],
        columns: [{
         title: 'CODE',
         data: 'trading_code',
@@ -63,10 +69,12 @@ export class HomeTableComponent implements OnInit {
     
     };
 
-    //  this.getJSON().subscribe(data => {
-    //    this.dtOptions.data = data
-    //    this.dataAvail = true
-    //   });
+     this.getJSON().subscribe(data => {
+      data = data.filter(function(dat:any){return dat.trading_code!="";}) 
+      this.dtOptions.data = data
+
+       this.dataAvail = true
+      });
    }
 
    public getJSON(): Observable<any> {
