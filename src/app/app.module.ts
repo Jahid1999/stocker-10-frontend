f521f50d8ee596fff8ac75cf799f0aa4e225e2da
Ashraful Gafur <ashraful.gafur@streamstech.com>
2022-08-19 22:29:55 +0600

    problem resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,49 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { DataTablesModule } from 'angular-datatables';
+import { HomeTableComponent } from './home-table/home-table.component';
+import { BrowserModule } from '@angular/platform-browser';
+import {NgApexchartsModule} from "ng-apexcharts";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataTablesModule } from 'angular-datatables';
-import { HomeTableComponent } from './home-table/home-table.component';
-
+import { PieChartComponent } from './features/home/components/pie-chart/pie-chart.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import { HomeModule } from './features/home/home.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeTableComponent
+    HomeTableComponent,
+    PieChartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    DataTablesModule
+    DataTablesModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgApexchartsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatTableModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
