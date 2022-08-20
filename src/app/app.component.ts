7c023600893c7c6a90eab41b117e421ae2b650af
atkia <bsse1015@iit.du.ac.bd>
2022-08-20 09:56:47 +0600

    button addded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Stocker-10-Frontend';
+  state=0;
+  today=true;
+  compare:boolean|any;
+  gain:boolean|any;
+  setState(x:number,s:string){
+    this.state = x;
+    if(s=='today')
+    {
+      this.today = true;
+      this.compare = false;
+      this.gain = false;
+    }
+    else if(s=='compare'){
+      this.compare = true;
+      this.today = false;
+      this.gain = false;
+
+    }
+    else{
+      this.gain = true;
+    }
+
+  }
 }
