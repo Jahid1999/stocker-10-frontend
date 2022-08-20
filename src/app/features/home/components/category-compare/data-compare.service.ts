a13940dbf4ed9537a5a505e86ee9d19cb72a6dce
mh-sun <bsse1025@iit.du.ac.bd>
2022-08-20 11:32:42 +0600

    graph merged

---
+++ src/app/features/home/components/category-compare/data-compare.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { DataCompare } from './data.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataCompareService {
+
+  private url:string = 'http://20.237.1.65/api/cat_compare/'
+  constructor(private http:HttpClient){}
+
+  getCompareData()
+  {
+    return this.http.get<DataCompare[]>(this.url).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+}
