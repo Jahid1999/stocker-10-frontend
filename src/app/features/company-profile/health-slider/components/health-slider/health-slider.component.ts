ea6431bd6b9bc47d4a0e813a78cfcd0d18661f62
mahdeehasanbd1010 <bsse1010@iit.du.ac.bd>
2022-10-05 09:11:22 +0600

    add health indicator with api

---
+++ src/app/features/company-profile/health-slider/components/health-slider/health-slider.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import {HealthIndicatorModel} from "../../models/HealthIndicatorModel";
+import {HealthSliderService} from "../../services/health-slider.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-health-slider',
+  templateUrl: './health-slider.component.html',
+  styleUrls: ['./health-slider.component.scss']
+})
+export class HealthSliderComponent implements OnInit {
+
+  public itemsQuantity: number = 0;
+  public itemWidth: number = 150;
+  public itemHeight: number = 100;
+
+  public healthIndicatorSliders: any = [];
+  public company_name: any;
+
+  public tempHealthIndicatorSliders: any = [
+    {
+      name: "EPS",
+      value: 98.69
+    },
+    {
+      name: "PE RATIO",
+      value: 24.64
+    },
+    {
+      name: "PEG",
+      value: 2.98
+    },
+    {
+      name: "DY",
+      value: 12.23
+    },
+  ];
+  public isLoad: boolean = false;
+
+  constructor(private route: ActivatedRoute,
+              private healthSliderService: HealthSliderService) { }
+
+
+  ngOnInit(): void {
+
+    this.company_name = this.route.snapshot.paramMap.get('company-name');
+    this.healthSliderService.getHealthSliderData(this.company_name).subscribe((response: any)=>{
+      if(response){
+        this.dataProcessingForSlider(response);
+        this.itemsQuantity = this.healthIndicatorSliders.length;
+        this.isLoad = true;
+      }
+    });
+  }
+
+
+  public dataProcessingForSlider(data: any){
+    for(let key in data){
+      let healthSlider: HealthIndicatorModel = new HealthIndicatorModel();
+      healthSlider.name = this.healthSliderService.getHealthIndicatorAbbreviation(key);
+      healthSlider.value = data[key];
+      this.healthIndicatorSliders.push(healthSlider);
+      this.isLoad = true;
+    }
+  }
+
+
+}
