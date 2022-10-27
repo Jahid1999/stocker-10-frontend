4cba1da3a23895d33c092fa13b7dcf61e3a6f2e4
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-10-27 22:51:58 +0600

    console log cleared

--- src/app/features/company-graph/components/stochastic-oscillator/stochastic-oscillator.component.ts
+++ src/app/features/company-graph/components/stochastic-oscillator/stochastic-oscillator.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { data } from 'jquery';
 import {
   ChartComponent,
@@ -13,12 +13,12 @@ import {
   ApexPlotOptions,
   ApexTitleSubtitle,
   ApexFill,
-  ApexLegend,ApexTooltip
-
-} from "ng-apexcharts";
-import { aciData, seriesData } from "./data-series";
+  ApexLegend,
+  ApexTooltip,
+} from 'ng-apexcharts';
+import { aciData, seriesData } from './data-series';
 import { StochasticOscillatorService } from './service/stochastic-oscillator.service';
-import * as moment from 'moment'
+import * as moment from 'moment';
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
@@ -33,81 +33,85 @@ export type ChartOptions = {
   plotOptions: ApexPlotOptions;
   fill: ApexFill;
   title: ApexTitleSubtitle;
-
 };
 
 @Component({
   selector: 'app-stochastic-oscillator',
   templateUrl: './stochastic-oscillator.component.html',
-  styleUrls: ['./stochastic-oscillator.component.scss']
+  styleUrls: ['./stochastic-oscillator.component.scss'],
 })
 export class StochasticOscillatorComponent implements OnInit {
-
-  public candlestickChart : Partial<ChartOptions>|any;
-  public oscillatorChart : Partial<ChartOptions>|any;
-  @Input() companyCode ='';
+  public candlestickChart: Partial<ChartOptions> | any;
+  public oscillatorChart: Partial<ChartOptions> | any;
+  @Input() companyCode = '';
   dataReady = false;
-  kseries:any = [];
-  dseries:any = [];
+  kseries: any = [];
+  dseries: any = [];
   date: any = [];
   ovb: any = [];
   ovs: any = [];
-  candleData:any = [];
-  constructor(private stochasticService: StochasticOscillatorService) { }
+  candleData: any = [];
+  constructor(private stochasticService: StochasticOscillatorService) {}
 
   ngOnInit(): void {
-
     this.loadCandleChartData();
     this.loadStochasticData();
   }
 
-  setPeriod(data:any){
-    for(let i=data.length-1;i>=(data.length-365);i--){
+  setPeriod(data: any) {
+    for (let i = data.length - 1; i >= data.length - 365; i--) {
       this.date.push(data[i].DateEpoch);
-
     }
   }
 
-  setKSeries(data:any){
-    for(let i=data.length-1;i>=(data.length-365);i--){
-      this.kseries.push(data[i-365].Stochastic);
+  setKSeries(data: any) {
+    for (let i = data.length - 1; i >= data.length - 365; i--) {
+      this.kseries.push(data[i - 365].Stochastic);
     }
   }
 
-  setDSeries(data: any){
-    for(let i=2;i<this.kseries.length;i++){
+  setDSeries(data: any) {
+    for (let i = 2; i < this.kseries.length; i++) {
       //let d = (this.kseries[i]+this.kseries[i-1]+this.kseries[i-2])/3;
-      this.dseries.push((this.kseries[i]+this.kseries[i-1]+this.kseries[i-2])/3);
-      this.ovs.push(20); this.ovb.push(80);
+      this.dseries.push(
+        (this.kseries[i] + this.kseries[i - 1] + this.kseries[i - 2]) / 3
+      );
+      this.ovs.push(20);
+      this.ovb.push(80);
     }
   }
-  loadStochasticData(){
-    this.stochasticService.getStochasticData(this.companyCode).subscribe((data:any)=>{
-      this.setPeriod(data);
-      this.setKSeries(data);
-      this.setDSeries(data);
+  loadStochasticData() {
+    this.stochasticService
+      .getStochasticData(this.companyCode)
+      .subscribe((data: any) => {
+        this.setPeriod(data);
+        this.setKSeries(data);
+        this.setDSeries(data);
 
-      this.setOscillatorChart(this.kseries, this.dseries)
-    })
-
+        this.setOscillatorChart(this.kseries, this.dseries);
+      });
   }
-  findK_series(){
-    let max=0, min=9000;
+  findK_series() {
+    let max = 0,
+      min = 9000;
     let kdata = [];
     let dData = [];
 
-    for(let i=0;i<seriesData.length;i++){
-      max = seriesData[i].y[1]>max ? seriesData[i].y[1]: max;
-      min = seriesData[i].y[2]<min ? seriesData[i].y[2]: min;
+    for (let i = 0; i < seriesData.length; i++) {
+      max = seriesData[i].y[1] > max ? seriesData[i].y[1] : max;
+      min = seriesData[i].y[2] < min ? seriesData[i].y[2] : min;
 
-      if(i>=13){
-        let k = ((seriesData[i].y[3] - min)/(max-min)) * 100
-        kdata.push({"date": seriesData[i].x, "k-value": k})
+      if (i >= 13) {
+        let k = ((seriesData[i].y[3] - min) / (max - min)) * 100;
+        kdata.push({ date: seriesData[i].x, 'k-value': k });
       }
-
     }
-    for(let i=2;i<kdata.length;i++){
-      let d = (kdata[i]['k-value']+kdata[i-1]['k-value']+kdata[i-2]['k-value'])/3;
+    for (let i = 2; i < kdata.length; i++) {
+      let d =
+        (kdata[i]['k-value'] +
+          kdata[i - 1]['k-value'] +
+          kdata[i - 2]['k-value']) /
+        3;
       dData.push(d);
     }
     // dData.forEach(element => {
@@ -116,11 +120,10 @@ export class StochasticOscillatorComponent implements OnInit {
     // kdata.forEach(element => {
     //   console.log(element)
     // });
-    this.setOscillatorChart(kdata, dData)
+    this.setOscillatorChart(kdata, dData);
   }
 
-
-  setOscillatorChart(kdata:any, dData: any){
+  setOscillatorChart(kdata: any, dData: any) {
     // let kvalue: any = [];
     // let date: any = [];
     // kdata.forEach((element: any) => {
@@ -129,43 +132,43 @@ export class StochasticOscillatorComponent implements OnInit {
     // });
     // console.log(kvalue)
     this.oscillatorChart = {
-      series:[
+      series: [
         {
-          name : "%K",
-          data: kdata.slice(2)
+          name: '%K',
+          data: kdata.slice(2),
         },
         {
-          name: "%D",
-          data: dData
+          name: '%D',
+          data: dData,
         },
 
         {
-          name: "Overbought",
+          name: 'Overbought',
           data: this.ovb,
         },
         {
-          name: "Oversold",
+          name: 'Oversold',
           data: this.ovs,
         },
       ],
       chart: {
-        type: "line",
+        type: 'line',
         height: 350,
         //width: 720
       },
       title: {
-        text: "Stochastic Oscilator (14-day period)",
-        align: "center"
+        text: 'Stochastic Oscilator (14-day period)',
+        align: 'center',
       },
-      colors:['#4287f5','#fc7b03','#ff0000','#0a6b15'],
+      colors: ['#4287f5', '#fc7b03', '#ff0000', '#0a6b15'],
       stroke: {
-        width: [2,2,1,1],
-        dashArray: [0, 0, 4,4]
+        width: [2, 2, 1, 1],
+        dashArray: [0, 0, 4, 4],
       },
       legend: {
         show: true,
         position: 'top',
-        horizontalAlign:'right',
+        horizontalAlign: 'right',
         markers: {
           width: 40,
           height: 8,
@@ -174,86 +177,88 @@ export class StochasticOscillatorComponent implements OnInit {
           customHTML: undefined,
           onClick: undefined,
           offsetX: 0,
-          offsetY: 0
-      },
-
+          offsetY: 0,
+        },
       },
       xaxis: {
-        type: "datetime",
-        categories:this.date.slice(2)
+        type: 'datetime',
+        categories: this.date.slice(2),
       },
-      yaxis: [{
-        labels: {
-            formatter: function (val:any) {
-                if(val) return val.toFixed(2)
-            }
+      yaxis: [
+        {
+          labels: {
+            formatter: function (val: any) {
+              if (val) return val.toFixed(2);
+            },
+          },
+          tooltip: {
+            enabled: true,
+          },
         },
-        tooltip: {
-          enabled: true
-        }
-    }]
-    }
-
+      ],
+    };
   }
-  loadCandleChartData(){
-    this.stochasticService.getCandleData(this.companyCode).subscribe((data:any)=>{
-      this.candleData = data;
-      this.candleData.forEach((element:any) => {
-        let momObject = moment(element["x"], "DD/MM/YYYY");
-        element.x = (momObject.toDate());
-        element.x.setDate(element.x.getDate()+1);
-        // console.log(new Date(element["x"]))
-        // element.x = new Date(element["x"])
-        //element.x = [element["x"].getFullYear(),element["x"].getMonth()+1,element["x"].getDate()].join('-')
-       });
-      this.candleData = this.candleData.slice(this.candleData.length-365);
-      this.setCandleChart(this.candleData);
-    })
+  loadCandleChartData() {
+    this.stochasticService
+      .getCandleData(this.companyCode)
+      .subscribe((data: any) => {
+        this.candleData = data;
+        this.candleData.forEach((element: any) => {
+          let momObject = moment(element['x'], 'DD/MM/YYYY');
+          element.x = momObject.toDate();
+          element.x.setDate(element.x.getDate() + 1);
+          // console.log(new Date(element["x"]))
+          // element.x = new Date(element["x"])
+          //element.x = [element["x"].getFullYear(),element["x"].getMonth()+1,element["x"].getDate()].join('-')
+        });
+        this.candleData = this.candleData.slice(this.candleData.length - 365);
+        this.setCandleChart(this.candleData);
+      });
     //this.setCandleChart(aciData);
   }
-  setCandleChart(seriesData:any){
-
-
-   console.log(seriesData.length)
-  //  seriesData.forEach((element:any) => {
-  //   let momObject = moment(element["x"], "DD/MM/YYYY");
-  //   element.x = (momObject.toDate());
-  //   element.x.setDate(element.x.getDate()+1);
-  //   // console.log(new Date(element["x"]))
-  //   // element.x = new Date(element["x"])
-  //   //element.x = [element["x"].getFullYear(),element["x"].getMonth()+1,element["x"].getDate()].join('-')
-  //  });
+  setCandleChart(seriesData: any) {
+    //  console.log(seriesData.length)
+    //  seriesData.forEach((element:any) => {
+    //   let momObject = moment(element["x"], "DD/MM/YYYY");
+    //   element.x = (momObject.toDate());
+    //   element.x.setDate(element.x.getDate()+1);
+    //   // console.log(new Date(element["x"]))
+    //   // element.x = new Date(element["x"])
+    //   //element.x = [element["x"].getFullYear(),element["x"].getMonth()+1,element["x"].getDate()].join('-')
+    //  });
 
     this.candlestickChart = {
-      series : [{
-        name : "Price",
-        type: "candlestick",
-        data : seriesData,
-      },
+      series: [
+        {
+          name: 'Price',
+          type: 'candlestick',
+          data: seriesData,
+        },
       ],
       chart: {
-        type: "candlestick",
+        type: 'candlestick',
         height: 350,
-
       },
       title: {
-        text: "Stock price",
-        align: "center"
+        text: 'Stock price',
+        align: 'center',
       },
       xaxis: {
-        type: "datetime",
+        type: 'datetime',
       },
-      yaxis: [{
-        labels: {
-            formatter: function (val:any) {
-                if(val) return val.toFixed(2)
-            }
+      yaxis: [
+        {
+          labels: {
+            formatter: function (val: any) {
+              if (val) return val.toFixed(2);
+            },
+          },
+          tooltip: {
+            enabled: true,
+          },
         },
-        tooltip: {
-          enabled: true
-        }
-    }]
-    }
-    this.dataReady =true;
+      ],
+    };
+    this.dataReady = true;
   }
 }
