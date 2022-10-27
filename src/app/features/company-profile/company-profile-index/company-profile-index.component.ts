15ecdabfa4d4e24f6b70649dbc6503087ac6729d
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-10-27 22:12:38 +0600

    CSS fixed

--- src/app/features/company-profile/company-profile-index/company-profile-index.component.ts
+++ src/app/features/company-profile/company-profile-index/company-profile-index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-profile-index',
@@ -9,12 +9,16 @@ import { ActivatedRoute } from '@angular/router';
 export class CompanyProfileIndexComponent implements OnInit {
   public company_name: string = '';
 
-  constructor(private router: ActivatedRoute) {}
+  constructor(private router: ActivatedRoute, private ACrouter: Router) {}
 
   ngOnInit(): void {
     this.company_name = this.router.snapshot.params['company-name'];
   }
   navigateToGraphPage() {
     // console.log(this.company_name);
+    let fullname = this.router.snapshot.params['full-name'];
+    this.ACrouter.navigateByUrl(
+      `profile/${this.company_name}/${fullname}/graph`
+    );
   }
 }
